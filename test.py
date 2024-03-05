87675ff20c380ade40f48315f1f6c4b41531bf61
Bill-Huangz <66486446+Bill-Huangz@users.noreply.github.com>
2024-03-05 17:33:22 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 import json
-headers ={'Authorization': 'Client-ID 09MFa-m1P_MfbElntIR0VkNmj9LhrDXkI1hRYKzDIcU'}
+headers ={'Authorization': 'Client-ID'}
 r = requests.get('https://api.unsplash.com/photos/random',headers=headers)
 
 print(r['urls'])
